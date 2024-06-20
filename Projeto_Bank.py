b83e05a418fc62f442f736474491d948276f040c
Vinicius Costa <viniciuscostauk@gmail.com>
2024-06-20 17:29:48 +0100

    primeiro commit

---
+++ Projeto_Bank.py
@@ -0,0 +1,73 @@
+
+
+menu = """
+
+========= COSTA BANK =========
+[1] Depositar
+[2] Sacar
+[3] Extrato
+[0] Sair
+==============================
+
+ """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+
+    opcao = input(menu)
+
+    if opcao == "1":
+        valor = float(input("Informe o valor do depósito: "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+            print(f"Depósito de R$ {valor:.2f} realizado com sucesso.")
+            
+        else:
+            print("Operação não realizada! O valor informado é inválido.")
+            
+    elif opcao == "2":
+        valor = float(input("Informe o valor do saque: "))
+        
+        excedeu_saldo = valor > saldo
+
+        excedeu_limite = valor > limite
+
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("Operação não realizada! Você não tem saldo suficiente.")
+
+        elif excedeu_limite:
+            print("Operação não realizada! O valor máximo por saque é 500.")
+            
+        elif excedeu_saques:
+            print ("Operação não realizada! Número máximo de saques excedido.")    
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            numero_saques += 1
+            print("Saque realizado com sucesso!")
+               
+        else:
+            print("Operação não realizada! O valor informado é inválido.")
+
+    elif opcao == "3":
+        print("\n================ EXTRATO COSTA BANK ================")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("=====================================================")
+
+    elif opcao == "0":
+        print("COSTA BANK AGRADECE!")
+        break
+
+    else:
+        print("Operação não realizada, por favor selecione novamente a operação desejada.")
---
+++ README.md
@@ -0,0 +1,17 @@
+
+# Projeto sistema bancário 💰 simples     
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54) 
+
+## Objetivo 
+Criei um sistem simples em python para
+listar todos os depósitos e saques realizados na conta, e será exibido as movimentações que o usuário estará operando no sitema. 
+
+## Passos
+
+1 -  Fazer um depósito de qualquer quantia;
+
+2 -  Limite diário de saque 3 vezes;
+
+3 - Limite de saque até  R$ 500,00; 
+
+4 -  Emissão de extrato quando o usuário desejar para checar as movimentações.
